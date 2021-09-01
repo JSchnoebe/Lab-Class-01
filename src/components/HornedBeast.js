41fd5cbfa99ad7ee7ab1d4c89763278e7b0211a9
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-09-01 16:33:23 -0500

    Added bootstrap and card

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -24,8 +25,16 @@ class HornedBeast extends React.Component {
     return (
       <div>
       <h2>{this.props.title}</h2>
-      <img src={this.props.image_url} alt="" />
-      <p>{this.props.description}</p>
+      <strong>{this.props.name} ❤️ = {this.state.votes} votes!</strong>
+      <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.props.image_url} onClick={this.addVoteClickHandler}/>
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+      {this.props.description}
+    </Card.Text>
+  </Card.Body>
+</Card>
       </div>
     )
   }
